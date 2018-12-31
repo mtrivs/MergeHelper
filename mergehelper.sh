560ef7710fa2fe07a7407727a6486b08de804581
mtrivs <mtrivs@gmail.com>
2018-12-31 20:45:51 +0000

    Updated Variables.  Begin command options and additional error handling.

--- mergehelper.sh
+++ mergehelper.sh
@@ -20,18 +20,39 @@
 # NOTE: This variable should not end with a forward slash '/'
 GAMEROOT="/home/mtrivs/games"
 #
-# You can optionally set the 'PYDIR' variable below, to absolute location of your python executable
+# If the 'DESTRUCTMODE' variable is left unchanged, the user will be prompted at the start of the script to determine if the original BIN/CUE files
+# should be deleted after a successfully binmerge operation. The variable can be optionally modified to one of the following values:
+# --------------------
+# DESTRUCTMODE="2" : [default] The user will be prompted each time the script is executed, to determine if original files should be removed
+# DESTRUCTMODE="0" : The script will not remove any original BIN/CUE files.   Instead, original files will be moved to a new 'orig' folder, created
+#                    in the same directory as the BIN/CUE files
+# DESTRUCTMODE="1" : The script to remove original BIN/CUE files, if the binmerge operation completes successfully
+DESTRUCTMODE="2"
+#
+# Optional: Set the 'PYDIR' variable below, to absolute location of your python executable
 # This must be python version 3!
 # If you do not set this variable, the script will call 'python3' by name
 PYDIR="python3"
 #
 # DO NOT EDIT BELOW THIS LINE
 # ____________________________________________________________________________________
-echo "*****************************************************"
 echo "~~~~~~~~~~~~~~~   MergeHelper v0.5  ~~~~~~~~~~~~~~~~~"
-echo "~~~~~~~~~~~~~~~    ~ by mtrivs ~    ~~~~~~~~~~~~~~~~~"
+echo "~~~~~~~~~~~~~~~    - by mtrivs -    ~~~~~~~~~~~~~~~~~"
 echo "~~~~~~~~~~~~~~  Powered by binmerge  ~~~~~~~~~~~~~~~~"
-echo "*****************************************************"
+# Query the user to determine if they wish to remove the original BIN/CUE files
+if [ $DESTRUCTMODE -eq "2" ]; then
+	echo "Destructmode is UNSET"
+	sleep 30
+elif [ $DESTRUCTMODE -eq "1" ]; then
+	echo "Destructmode is TRUE"
+	sleep 30
+elif [ $DESTRUCTMODE -eq "0" ]; then
+	echo "Destructmode is FALSE"
+	sleep 30
+else
+	echo "Unable to detect destructmode"
+	sleep 30
+fi
 # Abort if Python version 3 is not installed
 "$PYDIR" --version &> /dev/null
 if [ $? -ne 0 ]; then
@@ -86,33 +107,33 @@ for GAMEDIR in  "$GAMEROOT"/* ; do
                 gamename=$(basename "$GAMEDIR")
 
                 # Display the game name being processed
-                echo "Now processing folder: "$gamename""
+                echo "Now processing folder: "$GAMENAME""
 
                 # If there are no BIN files in the folder, skip it
                 if [ "$numbin" -eq 0 ]; then
-                        echo "...ERROR: unable to find any BIN files in "$gamename".  Skipping folder!"
+                        echo "...ERROR: unable to find any BIN files in "$GAMENAME".  Skipping folder!"
                         continue
                 fi
 
                 # If there are two or more BIN files, proceed with the merge process
                 if [ "$numbin" -gt 1 ]; then
-                        echo "...More than one BIN detected for "$gamename", attempting BinMerge!"
+                        echo "...More than one BIN detected for "$GAMENAME", attempting BinMerge!"
                         cuecheck=(`find "$GAMEDIR" -maxdepth 1 -name "*.[cC][uU][eE]" | wc -l`)
                         # Make sure there is only 1 CUE file inside the game directory
                         if [ "$cuecheck" -eq 1 ]; then
                                 cuename=`find "$GAMEDIR" -maxdepth 1 -type f -name "*.[cC][uU][eE]" -exec basename {} \;`
                                 echo "...CUE file found: "$cuename""
                         else
-                                echo "...ERROR: No CUE file or too mand CUE files detected! Skipping merge for "$gamename"..."
+                                echo "...ERROR: No CUE file or too mand CUE files detected! Skipping merge for "$GAMENAME"..."
                                 continue
                         fi
                         FAIL=0
                         # Make a backup ("bkup") directory inside the game directory or FAIL=1
-                        eval mkdir -p "\"$GAMEDIR/bkup\"" || FAIL=1
+                        eval mkdir -p "\"$GAMEDIR/orig\"" || FAIL=1
                         if [ "$FAIL" -lt 1 ]; then
                                 # No failure when creating bkup directory, now copy original BIN/CUE files to backup directory
                                 echo "...Backing up original BIN/CUE files!"
-                                eval mv "\"$GAMEDIR\""/*.{[cC][uU][eE],[bB][iI][nN]} "\"$GAMEDIR\""/bkup/ || FAIL=1
+                                eval mv "\"$GAMEDIR\""/*.{[cC][uU][eE],[bB][iI][nN]} "\"$GAMEDIR\""/orig/ || FAIL=1
                         else
                                 echo "...ERROR: Failed to create backup directory! Skipping file"
                                 continue
@@ -120,8 +141,8 @@ for GAMEDIR in  "$GAMEROOT"/* ; do
                         if [ "$FAIL" -lt 1 ]; then
                                 # No failure when copying .bin/.cue to bkup.  Begin merging BIN/CUE files
                                 echo "...Merging BIN files!"
-				cuename2=`find "$GAMEDIR"/bkup -maxdepth 1 -name "*.[cC][uU][eE]"`
-                                eval "$PYDIR" ./BinMerge.py "\"$cuename2\"" "\"$gamename\"" -o "\"$GAMEDIR\"/" || FAIL=1
+				NEWQUEUE=`find "$GAMEDIR"/orig -maxdepth 1 -name "*.[cC][uU][eE]"`
+                                eval "$PYDIR" ./BinMerge.py "\"$NEWQUEUE\"" "\"$GAMENAME\"" -o "\"$GAMEDIR\"/" || FAIL=1
                         else
                                 echo "...ERROR: Failed to backup BIN/CUE files! Skipping file"
                                 continue
@@ -129,19 +150,18 @@ for GAMEDIR in  "$GAMEROOT"/* ; do
                         if [ "$FAIL" -lt 1 ]; then
                                 # No failure in the conversion process. Uncomment below to remove backup directory after sucessful BIN merge
                                 echo "...BinMerge completed successfully!"
-                                #eval rm -r "\"$GAMEDIR\""/bkup
+                                #eval rm -r "\"$GAMEDIR\""/orig
                         else
                                 # Merge process failed. Uncomment below to remove any partial BIN/CUE files and restore original BIN/CUE from backup
-                                echo "...ERROR: Merge failed! Your original files are safe in a backup directory!"
+                                echo "...ERROR: Merge failed! Removing any partial files and moving original files back!"
                                 eval rm "\"$GAMEDIR\""/*.{[cC][uU][eE],[Bb][Ii][nN]} &> /dev/null
-                                eval mv "\"$GAMEDIR\""/bkup/*.{[cC][uU][eE],[Bb][Ii][nN]} "\"$GAMEDIR\"/" && eval rm -r "\"$GAMEDIR\""/bkup
+                                eval mv "\"$GAMEDIR\""/orig/*.{[cC][uU][eE],[Bb][Ii][nN]} "\"$GAMEDIR\"/" && eval rm -r "\"$GAMEDIR\""/orig
                                 continue
                         fi
                 else
                         # Game folder only has 1 BIN file, so there is nothing to merge.
-                        echo "..."$gamename" doesn't need to be merged!"
+                        echo "..."$GAMENAME" doesn't need to be merged!"
                 fi
         fi
 done
 echo "** FINISHED processing all directories! **"
-echo "******************************************"
